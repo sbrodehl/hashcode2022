84bb0214ad53ac498db9a16798c43a2db7db0a40
Sebastian Brodehl <foss@s14l.de>
2022-01-08 21:39:05 +0100

    Add a solver which produces a random solution.

---
+++ Practice Round/solver/random.py
@@ -0,0 +1,25 @@
+import logging
+import random
+
+from .basesolver import BaseSolver
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Random(BaseSolver):
+    """Solve the problem nice and steady!
+    """
+    def __init__(self, input_str):
+        super().__init__(input_str)
+
+    def solve(self):
+        """Compute a solution to the given problem.
+
+        Save everything in an internal state.
+
+        :return: True, if a solution is found, False otherwise
+        """
+        while len(self.solution) == 0:
+            self.solution = [i for i in self.data[1] if random.randint(0, 1) > 0]
+
+        return True
