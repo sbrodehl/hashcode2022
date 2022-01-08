b66b1ecce2035b7f670fe3078f8d036552b7a575
Daniel Franzen <dfranz@uni-mainz.de>
2022-01-08 19:33:20 +0100

    Add Gurobi ILP construction.

--- Practice Round/solver/lp.py
+++ Practice Round/solver/lp.py
@@ -8,7 +8,7 @@ from gurobipy import GRB
 from .basesolver import BaseSolver
 
 LOGGER = logging.getLogger(__name__)
-
+logging.getLogger("gurobipy").setLevel(logging.ERROR)  # prevent duplicate gurobi output to console
 
 class LP(BaseSolver):
     """Solve the problem nice and steady!
@@ -25,35 +25,35 @@ class LP(BaseSolver):
         """
         C = len(self.data[0])
         I = len(self.data[1])
-        ingredients = {i: idx for i, idx in zip(self.data[1], range(I))}
-
-        constraints = 0
-        for c in self.data[0]:
-            if len(c.likes) > 0:
-                constraints += 1
-            if len(c.dislikes) > 0:
-                constraints += 1
+        ingredients_list = list(self.data[1])
+        ingredients_dict = {i: idx for i, idx in zip(ingredients_list, range(I))}
 
         try:
             # Create a new model
             m = gp.Model("one-pizza")
-
-            x = m.addMVar(shape=tuple([I]), vtype=GRB.BINARY, name="pizza")
+            var_c = m.addVars(C, vtype=GRB.BINARY, name='c')
+            var_i = m.addVars(I, vtype=GRB.BINARY, name='i')
 
             # Set objective
-            m.setObjective(..., GRB.MAXIMIZE)
+            score = var_c.sum()
+            m.setObjective(score, GRB.MAXIMIZE)
 
             # add constraints
-            ...
+            for v, client in zip(var_c.values(), self.data[0]):
+                m.addConstrs(v <=   var_i[ingredients_dict[i]] for i in client.likes   )
+                m.addConstrs(v <= 1-var_i[ingredients_dict[i]] for i in client.dislikes)
 
             # Optimize model
+            m.setParam(GRB.Param.MIPGap, 0)  # try to solve optimally
             m.optimize()
 
-            for v in m.getVars():
-                print('%s %g' % (v.varName, v.x))
+            #for v in m.getVars():  # (produces too large output)
+            #    print('%s %g' % (v.varName, v.x))
 
             print('Obj: %g' % m.objVal)
-
+            
+            self.solution = [ingredients_list[i] for i,v in var_i.items() if v.x]
+            
         except gp.GurobiError as e:
             print('Error code ' + str(e.errno) + ': ' + str(e))
             return False
